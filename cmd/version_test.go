e4ef5fc2793bad164eab94606dede16d2f24d6b9
mo3789530 <mo3789530@gmail.com>
2023-02-11 18:08:28 +0900

    add test

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -1,1 +1,29 @@
 package cmd
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestVersion(t *testing.T) {
+	t.Run("will show version", func(t *testing.T) {
+		args := []string{}
+		cmd := NewVersionCommand()
+
+		c := bytes.NewBufferString("")
+		cmd.SetArgs(args)
+		cmd.SetOut(c)
+		cmd.Execute()
+		out, err := io.ReadAll(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := "argocd-tf-plugin 0.0.1-rc"
+		if !strings.Contains(string(out), expected) {
+			t.Fatalf("expected to contain: %s but got %s", expected, out)
+		}
+	})
+}
