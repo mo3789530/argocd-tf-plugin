0a3917a9237ec37bebe1c95a0f6e60d969ad3829
mo3789530 <mo3789530@gmail.com>
2023-02-11 18:10:45 +0900

    update root

--- cmd/root.go
+++ cmd/root.go
@@ -31,12 +31,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "argocd-tf-plugin",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -52,14 +46,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.argocd-tf-plugin.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.AddCommand(NewVersionCommand())
 }
