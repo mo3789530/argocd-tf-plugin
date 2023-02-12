373e52c51bfc068dda15f12bc349621b784ba284
mo3789530 <mo3789530@gmail.com>
2023-02-12 18:47:21 +0900

    run tf command by cli

--- cmd/tf.go
+++ cmd/tf.go
@@ -4,13 +4,17 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	myhcl "argocd-tf-plugin/pkg/hcl"
+	"argocd-tf-plugin/pkg/terraform"
 	"fmt"
+	"log"
+	"regexp"
 
+	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/spf13/cobra"
 )
 
 func NewTfCommand() *cobra.Command {
-	const StdIn = "-"
 	var command = &cobra.Command{
 		Use:   "tf <action>",
 		Short: "Run argocd-tf-plugin",
@@ -21,13 +25,94 @@ func NewTfCommand() *cobra.Command {
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			path := args[0]
-			if path == StdIn {
-				
-			} 
+			fmt.Println(args)
+			dir, _ := cmd.Flags().GetString("dir")
+			relative, _ := cmd.Flags().GetString("relative")
+
+			switch args[0] {
+			case "plan":
+				return runPlan(dir, relative)
+			case "apply":
+				return runApply(dir, relative)
+			default:
+				log.Printf("%s not found \n", args[0])
+			}
+
 			return nil
 		},
 	}
 
+	command.Flags().StringP("dirs", "d", "", "working dir")
+	command.Flags().StringP("relative", "r", "", "relative")
+
 	return command
+}
+
+func runPlan(dir string, relative string) error {
+	folderpath := relative + "/" + dir
+	tf, exec, err := initTf(folderpath)
+
+	if err != nil {
+		log.Printf("error terraform init %s", err)
+		return err
+	}
+
+	isdiffer, err := exec.Plan(tf)
+	if err != nil {
+		log.Printf("err plan %s", err)
+		return err
+	}
+	if !isdiffer {
+		log.Printf("no changes")
+	} else {
+		show, err := exec.Show(tf, false)
+		if err != nil {
+			log.Printf("err show %s", err)
+			return err
+		}
+		log.Println(show)
+	}
+
+	return nil
+}
+
+func runApply(dir string, relative string) error {
+	folderpath := relative + "/" + dir
+	tf, exec, err := initTf(folderpath)
+
+	if err != nil {
+		log.Printf("error terraform init %s", err)
+		return err
+	}
+
+	err = exec.Apply(tf)
+	if err != nil {
+		log.Printf("terraform apply error %s", err)
+		return err
+	}
+	return nil
+}
+
+func initTf(folderpath string) (*tfexec.Terraform, terraform.Exec, error) {
+	filepath := folderpath + "/terraform.tf"
+
+	log.Print(filepath)
+
+	strVersion, err := myhcl.GetVersions(filepath)
+	if err != nil {
+		log.Fatalf("err %s", err)
+		return nil, nil, err
+	}
+	rex := regexp.MustCompile("[0-9.]+")
+	version := rex.FindString(strVersion)
+
+	log.Printf("%s using terrafrom version: %s", filepath, version)
+
+	exec := terraform.NewExec(version, true)
+	tf, err := exec.Init(folderpath)
+	if err != nil {
+		log.Printf("error init %s", err)
+		return nil, nil, err
+	}
+	return tf, exec, nil
 }
--- go.mod
+++ go.mod
@@ -5,14 +5,19 @@ go 1.19
 require (
 	github.com/hashicorp/go-version v1.6.0
 	github.com/hashicorp/hc-install v0.5.0
+	github.com/hashicorp/hcl/v2 v2.16.0
 	github.com/hashicorp/terraform-exec v0.17.3
+	github.com/spf13/cobra v1.6.1
 )
 
 require (
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/terraform-json v0.14.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/zclconf/go-cty v1.12.1 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
